a90b3a7ec3030a8dfb3bfb3d31c5d190474a8866
fnilvuwu <mamatmks45@gmail.com>
2024-05-21 16:27:10 +0800

    new file:   177. Nth Highest Salary.py

---
+++ 177. Nth Highest Salary.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    if (N <= 0):
+        query = None
+        output = pd.DataFrame([[query]], columns=[f"getNthHighestSalary({N})"])
+    else:
+        df_sort_by_salary = employee.drop_duplicates(subset='salary').sort_values('salary', ascending=False)
+        print(df_sort_by_salary)
+        try:
+            query = df_sort_by_salary.iloc[N - 1].salary
+            output = pd.DataFrame([[query]], columns=[f"getNthHighestSalary({N})"])
+        except IndexError:
+            query = None
+            output = pd.DataFrame([[query]], columns=[f"getNthHighestSalary({N})"])
+    
+    return output
